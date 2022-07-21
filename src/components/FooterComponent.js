8d30ad88c0baee57b547b3fd72489d4ea71adcc5
KmLig <phamxuankhiem1102@gmail.com>
2022-07-22 00:50:06 +0700

    modified footer styling

--- src/components/FooterComponent.js
+++ src/components/FooterComponent.js
@@ -5,7 +5,7 @@ function Footer(props) {
     <div className="footer bg-black text-white p-4">
       <div className="container">
         <div className="row justify-content-center">
-          <div className="col-4 offset-1 col-sm-2">
+          <div className="col-4 offset-1 col-sm-2  text-warning">
             <h5>Links</h5>
             <ul className="list-unstyled">
               <li>
@@ -19,7 +19,7 @@ function Footer(props) {
               </li>
             </ul>
           </div>
-          <div className="col-7 col-sm-5">
+          <div className="col-7 col-sm-5  text-warning">
             <h5>Our Address</h5>
             <address>
               1 TTP District 2
@@ -39,43 +39,43 @@ function Footer(props) {
           <div className="col-12 col-sm-4 align-self-center">
             <div className="text-center">
               <a
-                className="btn btn-social-icon btn-google"
+                className="btn btn-social-icon btn-google m-1"
                 href="http://google.com/+"
               >
                 <i className="fa fa-google-plus"></i>
               </a>
               <a
-                className="btn btn-social-icon btn-facebook"
+                className="btn btn-social-icon btn-facebook m-1"
                 href="http://www.facebook.com/profile.php?id="
               >
                 <i className="fa fa-facebook"></i>
               </a>
               <a
-                className="btn btn-social-icon btn-linkedin"
+                className="btn btn-social-icon btn-linkedin m-1" 
                 href="http://www.linkedin.com/in/"
               >
                 <i className="fa fa-linkedin"></i>
               </a>
               <a
-                className="btn btn-social-icon btn-twitter"
+                className="btn btn-social-icon btn-twitter m-1"
                 href="http://twitter.com/"
               >
                 <i className="fa fa-twitter"></i>
               </a>
               <a
-                className="btn btn-social-icon btn-google"
+                className="btn btn-social-icon btn-google m-1"
                 href="http://youtube.com/"
               >
                 <i className="fa fa-youtube"></i>
               </a>
-              <a className="btn btn-social-icon" href="mailto:">
+              <a className="btn btn-social-icon m-1" href="mailto:">
                 <i className="fa fa-envelope-o"></i>
               </a>
             </div>
           </div>
         </div>
         <div className="row justify-content-center">
-          <div className="col-auto">
+          <div className="col-auto text-warning">
             <p>©Copyright KP</p>
           </div>
         </div>
