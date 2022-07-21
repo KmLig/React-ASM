fa9657e10bb83bc371ca1c465f9f86d9003c85d2
KmLig <phamxuankhiem1102@gmail.com>
2022-07-22 00:39:23 +0700

    modified Staff Detail Card

--- src/components/StaffListComponent.js
+++ src/components/StaffListComponent.js
@@ -32,6 +32,9 @@ class StaffList extends Component {
       return (
         <Card className="mt-2">
           <div className="row">
+          <div className="col-3">
+              <CardImg className="" src={staff.image} alt={staff.name} />
+            </div>
             <div className="col-9 bg-dark text-warning rounded-3">
               <CardBody>
                 <CardTitle>
@@ -47,10 +50,7 @@ class StaffList extends Component {
                 <CardText>Số ngày nghỉ còn lại: {staff.annualLeave}</CardText>
                 <CardText>Số ngày đã làm thêm: {staff.overTime}</CardText>
               </CardBody>
-            </div>
-            <div className="col-3">
-              <CardImg className="" src={staff.image} alt={staff.name} />
-            </div>
+            </div>            
           </div>
         </Card>
       );
