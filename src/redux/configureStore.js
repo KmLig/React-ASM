133c15c910f2c18f72dd8ab6eb2310c29476f04e
KmLig <phamxuankhiem1102@gmail.com>
2022-08-03 23:37:59 +0700

    modified Redux store

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,6 +1,8 @@
 import { createStore, combineReducers, applyMiddleware } from 'redux';
 import { Staffs } from './staffs';
 import { Departments } from './departments';
+import thunk from 'redux-thunk';
+import logger from 'redux-logger';
 
 
 export const ConfigureStore = () => {
