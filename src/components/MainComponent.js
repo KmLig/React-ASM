f8bc852fd994a12f3b9e35c6cdc9077e68ff8e41
KmLig <phamxuankhiem1102@gmail.com>
2022-08-05 00:14:19 +0700

    modified StaffDetail Component to retrieve department.name

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -1,67 +1,111 @@
-import React, { Component } from 'react';
-import Home from './HomeComponent';
-import StaffList from './StaffListComponent';
-import Department from './DepartmentComponent';
-import Header from './HeaderComponent';
-import Footer from './FooterComponent';
-import StaffDetail from './StaffDetailComponent';
-import Salary from './SalaryComponent';
-import SearchStaff from './SearchStaffComponent';
-import { Switch, Route, Redirect, withRouter } from 'react-router-dom';
-import { connect } from 'react-redux';
-import { addStaff, fetchDepartments, fetchStaffs } from '../redux/ActionCreators';
-import { actions } from 'react-redux-form';
+import React, { Component } from "react";
+import Home from "./HomeComponent";
+import StaffList from "./StaffListComponent";
+import Department from "./DepartmentComponent";
+import Header from "./HeaderComponent";
+import Footer from "./FooterComponent";
+import StaffDetail from "./StaffDetailComponent";
+import Salary from "./SalaryComponent";
+import SearchStaff from "./SearchStaffComponent";
+import { Switch, Route, Redirect, withRouter } from "react-router-dom";
+import { connect } from "react-redux";
+import {
+  addStaff,
+  fetchDepartments,
+  fetchStaffs,
+} from "../redux/ActionCreators";
+import { actions } from "react-redux-form";
 
-
-const mapStateToProps = state => {
+const mapStateToProps = (state) => {
   return {
     staffs: state.staffs,
-    departments: state.departments    
-  }
-}
+    departments: state.departments,
+  };
+};
 
 const mapDispatchToProps = (dispatch) => ({
-  addStaff: ({newStaff}) => dispatch(addStaff({newStaff})),
-  fetchStaffs: () => {dispatch(fetchStaffs())},
-  fetchDepartments: () => {dispatch(fetchDepartments())},
-  resetAddStaffForm: () => {dispatch(actions.reset('addStaff'))}
+  addStaff: ({ newStaff }) => dispatch(addStaff({ newStaff })),
+  fetchStaffs: () => {
+    dispatch(fetchStaffs());
+  },
+  fetchDepartments: () => {
+    dispatch(fetchDepartments());
+  },
+  resetAddStaffForm: () => {
+    dispatch(actions.reset("addStaff"));
+  },
 });
 
-class Main extends Component {      
+class Main extends Component {
   constructor(props) {
-    super(props);  
+    super(props);
   }
 
   componentDidMount() {
     this.props.fetchStaffs();
     this.props.fetchDepartments();
   }
-  
+
   render() {
-    const StaffWithId = ({match}) => {      
-      return(
-          <StaffDetail staff={this.props.staffs.staffs.filter((staff) => staff.id === parseInt(match.params.staffId,10))[0]} 
+    const StaffWithId = ({ match }) => {
+      return (
+        <StaffDetail
+          staff={
+            this.props.staffs.staffs.filter(
+              (staff) => staff.id === parseInt(match.params.staffId, 10)
+            )[0]
+          }
           isLoading={this.props.staffs.isLoading}
-          errMess={this.props.staffs.errMess}/>
+          errMess={this.props.staffs.errMess}
+          departments={this.props.departments.departments}
+        />
       );
-    };  
- 
-    return (           
-        <div>
-          <Header />
-          <Switch>
-              <Route path='/homepage' component={Home} />
-              <Route exact path='/employee' component={() => <StaffList staffs={this.props.staffs.staffs} addStaff={this.props.addStaff} departments={this.props.departments.departments} staffsLoading={this.props.staffs.isLoading} staffFailed={this.props.staffs.errMess} resetAddStaffForm={this.props.resetAddStaffForm} />} />
-              <Route path='/employee/:staffId' component={StaffWithId} />
-              <Route path='/searchstaff' component={() => <SearchStaff staff={this.props.staffs.staffs} />} />
-              <Route path='/department' component={() => <Department departments={this.props.departments.departments} isLoading={this.props.departments.isLoading} isFailed={this.props.departments.errMess}/>} />
-              <Route path='/salary' component={() => <Salary staffs={this.props.staffs.staffs} />} />
-              <Redirect to='/homepage' />
-          </Switch>
-          <Footer />
-        </div>         
+    };
+
+    return (
+      <div>
+        <Header />
+        <Switch>
+          <Route path="/homepage" component={Home} />
+          <Route
+            exact
+            path="/employee"
+            component={() => (
+              <StaffList
+                staffs={this.props.staffs.staffs}
+                addStaff={this.props.addStaff}
+                departments={this.props.departments.departments}
+                staffsLoading={this.props.staffs.isLoading}
+                staffFailed={this.props.staffs.errMess}
+                resetAddStaffForm={this.props.resetAddStaffForm}
+              />
+            )}
+          />
+          <Route path="/employee/:staffId" component={StaffWithId} />
+          <Route
+            path="/searchstaff"
+            component={() => <SearchStaff staff={this.props.staffs.staffs} />}
+          />
+          <Route
+            path="/department"
+            component={() => (
+              <Department
+                departments={this.props.departments.departments}
+                isLoading={this.props.departments.isLoading}
+                isFailed={this.props.departments.errMess}
+              />
+            )}
+          />
+          <Route
+            path="/salary"
+            component={() => <Salary staffs={this.props.staffs.staffs} />}
+          />
+          <Redirect to="/homepage" />
+        </Switch>
+        <Footer />
+      </div>
     );
-  }  
+  }
 }
 
-export default withRouter(connect(mapStateToProps, mapDispatchToProps)(Main));;
+export default withRouter(connect(mapStateToProps, mapDispatchToProps)(Main));
--- src/components/StaffDetailComponent.js
+++ src/components/StaffDetailComponent.js
@@ -1,15 +1,26 @@
 import React from "react";
 import dateFormat from "dateformat";
-import { Card, CardImg, CardText, CardBody, CardTitle, Breadcrumb, BreadcrumbItem } from "reactstrap";
-import { Link } from 'react-router-dom';
-import { Loading } from './LoadingComponent';
+import {
+  Card,
+  CardImg,
+  CardText,
+  CardBody,
+  CardTitle,
+  Breadcrumb,
+  BreadcrumbItem,
+} from "reactstrap";
+import { Link } from "react-router-dom";
+import { Loading } from "./LoadingComponent";
 
-function RenderStaff({ staff }) {
-  if (staff != null)
+function RenderStaff({ staff, departments }) {
+  if (staff != null) {
+    const department = departments.find(
+      (department) => staff.departmentId === department.id
+    );
     return (
       <Card key={staff.id} className="mt-2">
         <div className="row">
-        <div className="col-12 col-md-4 col-lg-3 p-0">
+          <div className="col-12 col-md-4 col-lg-3 p-0">
             <CardImg className="img-fluid" src={staff.image} alt={staff.name} />
           </div>
           <div className="col-12 col-md-8 col-lg-9 bg-dark text-warning rounded-3">
@@ -23,42 +34,48 @@ function RenderStaff({ staff }) {
               <CardText>
                 Ngày vào công ty: {dateFormat(staff.startDate, "dd/mm/yyyy")}
               </CardText>
-              <CardText>Phòng ban: {staff.departmentId}</CardText>
+              <CardText>
+                Phòng ban: {
+                 department && department.name         
+                }
+              </CardText>
               <CardText>Số ngày nghỉ còn lại: {staff.annualLeave}</CardText>
               <CardText>Số ngày đã làm thêm: {staff.overTime}</CardText>
             </CardBody>
-          </div>          
+          </div>
         </div>
       </Card>
     );
+  }
+
   else return <div></div>;
 }
 const StaffDetail = (props) => {
   if (props.isLoading) {
-    return(
-     <Loading />
-    )
-  }
-  else if (props.errMess) {
-    return(
+    return <Loading />;
+  } else if (props.errMess) {
+    return (
       <div className="container">
         <div className="row">
           <h4>{props.errMess}</h4>
         </div>
       </div>
-    )
-  }
-  else if (props.staff != null) {
+    );
+  } else if (props.staff != null) {
     return (
       <div className="container">
         <div className="row">
-            <hr className="mt-3"/>
-            <Breadcrumb>      
-                <BreadcrumbItem><Link to='/home'>Home</Link></BreadcrumbItem>      
-                <BreadcrumbItem><Link to="/employee">Staff list</Link></BreadcrumbItem>
-                <BreadcrumbItem active>{props.staff.name}</BreadcrumbItem>
-            </Breadcrumb>
-            <RenderStaff staff={props.staff} />
+          <hr className="mt-3" />
+          <Breadcrumb>
+            <BreadcrumbItem>
+              <Link to="/home">Home</Link>
+            </BreadcrumbItem>
+            <BreadcrumbItem>
+              <Link to="/employee">Staff list</Link>
+            </BreadcrumbItem>
+            <BreadcrumbItem active>{props.staff.name}</BreadcrumbItem>
+          </Breadcrumb>
+          <RenderStaff staff={props.staff} departments={props.departments} />
         </div>
       </div>
     );
